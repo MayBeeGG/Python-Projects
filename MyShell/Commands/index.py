e029279d988603bfc96f96f5ab0b7657b9fc9951
MayBee <171178489+MayBeeGG@users.noreply.github.com>
2025-09-24 19:59:53 +0200

    Added Simple Portscan module to shell

--- MyShell/Commands/index.py
+++
@@ -1,9 +0,0 @@
-class Command:
-    name: str
-    description: str
-
-    def __init__(self, name: str, description: str,suggestions: list[str]) -> None:
-        self.name = name
-        self.description = description
-        self.suggestions = suggestions # add suggestion logic
-
--- MyShell/main.py
+++ MyShell/main.py
@@ -1,12 +1,15 @@
 import pyfiglet
 import random
+from Module import *
+import subprocess
 # import readline
-#from autocomplete import function_completer
 
 possible_fonts = ["slant","epic","doom","speed"]
-path="" #! Should initialize with current location
 module=""
-command_dict = {}
+command_dict = {
+    "load": "Commands/load.py",
+}
+module_list = ["portscan"]
 
 # Colors for Console Output
 RED = '\033[91m'
@@ -19,22 +22,51 @@ RESET = '\033[0m'
 # readline.parse_and_bind("tab: complete")
 
 
-def default_line(path:str,module:str)->str:
+def default_line(module:str)->str:
     if module == "":
         return f"{GREEN} ${BLUE}>{RESET}"
     else:
-        return f"{RED}"+module+f"{GREEN} ${BLUE}>{RESET}"
+        return f"{RED}"+module+f"{GREEN}${BLUE}>{RESET}"
 
 
 
 if __name__ == "__main__":
     pyfiglet.print_figlet("MYSHELL \n", font=possible_fonts[random.randint(0, 3)])
     while True:
-        user_input = input(default_line(path, module))
+        user_input = input(default_line(module))
         if user_input.lower() in ["exit"]:
+            if module != "":
+                module = ""
+                continue
             print("Goodbye!")
             break
+        elif user_input == "":
+            continue
         else:
-            # Logic here
-            pass
+            if module == "":
+                try:
+                    user_input_split = user_input.split(" ")
+                    choosen_command = user_input_split[0]
+                    if choosen_command in command_dict.keys():
+                        try:
+                            choosen_module = user_input_split[1]
+                            if choosen_module in module_list:
+                                module = choosen_module
+                                print(f"Module {module} loaded")
+                                subprocess.run(["python", f"./MOdule/{module}.py"])
+                            else:
+                                print("Unknown module")
+                                continue
+                        except IndexError:
+                            print("Unknown command")
+                            continue
+                            
+                    else:
+                        print("Unknown command")
+                        continue
 
+                except Exception as e:
+                    print("Error processing input")
+                    continue
+            else:
+                print("Error processing input")
